c0eabac2e4341e0da4d76f6510e10f6f91234411
Eugene <ti.eugene@gmail.com>
2021-04-21 01:05:47 +0300

    RocksDB added (dirty; segfault on 19+)

---
+++ src/rdb.cpp
@@ -0,0 +1,35 @@
+// RocksDB
+#ifdef ROCKSDB
+#include "common.h"
+#include <rocksdb/db.h>
+#include <rocksdb/options.h>
+
+using namespace ROCKSDB_NAMESPACE;
+
+DB* db;
+
+bool DbOpen(void) {
+  Options options;
+  options.create_if_missing = true;
+  return DB::Open(options, "kvtest.rdb", &db).ok();
+}
+
+bool RecordAdd(const uint160_t &k, const uint32_t v) {
+  return db->Put(WriteOptions(), string_view((const char *) &k, sizeof(k)), string_view((const char *)&v, sizeof(v))).ok();
+}
+
+bool RecordGet(const uint160_t &k) {
+  std::string value;
+   return db->Get(ReadOptions(), string_view((const char *) &k, sizeof(k)), &value).ok();
+}
+
+int RecordGetOrAdd(const uint160_t &k, const uint32_t v) {
+    if (RecordGet(k))
+        return -1;
+    return int(RecordAdd(k, v));
+}
+
+int main(int argc, char *argv[]) {
+  return mainloop(argc, argv, DbOpen, RecordAdd, RecordGet, RecordGetOrAdd);
+}
+#endif
