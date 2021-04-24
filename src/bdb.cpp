d2b9a600f25d5b34ed2f1c3ea1c5764ed74985ad
Eugene <ti.eugene@gmail.com>
2021-04-24 13:36:19 +0300

    CLI ok (fixed #10, fixed 13)

--- src/bdb.cpp
+++ src/bdb.cpp
@@ -3,16 +3,29 @@
 #ifdef USE_BDB
 #include "common.h"
 #include <string_view>
+#include <map>
+#include <iterator>
 #include <db_cxx.h>
 
-const string_view DBNAME("kvtest.bdb");
+const map<string, DBTYPE> exts = {
+  {".bdh", DB_HASH},
+  {".bdt", DB_BTREE}
+};
+//{".bdq", DB_QUEUE},
+//{".bdr", DB_RECNO},
+const string DBNAME("kvtest.bdh");
+const string help = "\
+.bdh: DB_HASH\n\
+.bdt: DB_BTREE\
+";
 
 static Db *db = nullptr;
 
-bool db_open(void) {
+bool db_open(const string_view name, const DBTYPE type) {
+  // TODO: use DB_UNKNOWN on reopening to detect type
   if (!db)
     db = new Db(nullptr, 0);
-  return db->open(nullptr, DBNAME.cbegin(), nullptr, DB_HASH, DB_CREATE|DB_TRUNCATE, 0644) == 0;
+  return db->open(nullptr, name.cbegin(), nullptr, type, DB_CREATE|DB_TRUNCATE, 0644) == 0;
 }
 
 bool RecordAdd(const uint160_t &k, const uint32_t v) {
@@ -30,9 +43,21 @@ int RecordTry(const uint160_t &k, const uint32_t v) {
 }
 
 int main(int argc, char *argv[]) {
+  string name = DBNAME;
+  DBTYPE type = DB_HASH;
+  auto search = exts.end();
+
   if (!cli(argc, argv))
     return 1;
-  if (!db_open())
+  if (!dbname.empty()) {
+    auto l = dbname.length();
+    if ((l >= 4) and ((search = exts.find(dbname.substr(l - 4))) != exts.end())) {
+          name = dbname;
+          type = search->second;
+    } else
+      return ret_err("Filename must be *.ext, where 'ext' can be:\n" + help, 2);
+  }
+  if (!db_open(name, type))
     return ret_err("Cannot create db", 1);
   stage_add(RecordAdd);
   db->sync(0);
--- src/common.h
+++ src/common.h
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <array>
 #include <functional>
+#include <unistd.h>   // getopt
 
 using namespace std;
 
@@ -13,7 +14,18 @@ static uint160_t *buffer;
 static uint32_t RECS_QTY = 1 << 20;
 static uint32_t TESTS_QTY = 1 << 20;
 static uint64_t t1, t2, t3, t4, kops1, kops2, kops3, kops4;
-static bool test_get = true, test_ask = true, test_try = true;
+static bool verbose = false, test_get = true, test_ask = true, test_try = true;
+static string dbname;
+
+static string_view help_txt = "\
+Usage: [options] log2(records) (0..31))\n\
+Options:\n\
+-h        - this help\n\
+-n <path> - name of DB file/dir\n\
+-t n      - log2(tests), 1..31 (default=20)\n\
+-x (s)    - exclude steps (g=Get/a=Ask/t=Try)\n\
+-v        - verbose\
+";
 
 int ret_err(string_view msg, int err) {
   cerr << msg << endl;
@@ -22,40 +34,69 @@ int ret_err(string_view msg, int err) {
 
 bool cli(int argc, char *argv[])
 {
-    /*
-     * Process CLI
-     * Returns: success
-     */
-  bool retvalue = false;
+  /*
+   * Process CLI
+   * Returns: success
+   */
+  int opt, i;
 
-  if ((argc < 2) or (argc > 3))
-    cerr << "Usage: " << argv[0] << " <log2(records) (0..31)> [log2(tests) (0..31, default=" << TESTS_QTY << ")]" << endl;
-  else {
-    auto i = atoi(argv[1]);
-    if ((i < 1) or (i > 31))
-      cerr << "Bad log2(records): " << argv[1] << endl;
-    else {
-      RECS_QTY = 1 << i;
-      if (argc == 3) {
-        i = atoi(argv[2]);
-        if ((i < 1) or (i > 31))
-          cerr << "Bad log2(tests): " << argv[2] << endl;
-        else {
-          TESTS_QTY = 1 << i;
-          retvalue = true;
+  while ((opt = getopt(argc, argv, "hvn:t:x:")) != -1) {
+    switch (opt) {
+      case 'h':
+        cerr << help_txt << endl;
+        return false;
+      case 'v':
+        verbose = true;
+        break;
+      case 't':
+        i = atoi(optarg);
+        if ((i < 1) or (i > 31)) {
+            cerr << "-t must be 1..31" << endl;
+            return false;
         }
-      } else
-        retvalue = true;
+        TESTS_QTY = 1 << i;
+        break;
+      case 'n':   // name
+        dbname = optarg;
+        break;
+      case 'x':   // exclude
+        switch (*optarg) {
+          case 'g':
+            test_get = false;
+            break;
+          case 'a':
+            test_ask = false;
+            break;
+          case 't':
+            test_try = false;
+            break;
+          default:
+            cerr << "-x must be g|a|t" << endl;
+            return false;
+        }
+        break;
+      case '?':   // can handle optopt
+        cerr << help_txt << endl;
+        return false;
     }
   }
-  return (retvalue and (buffer = new uint160_t[RECS_QTY]));
+  // cerr << "v=" << verbose << ", t=" << TESTS_QTY << ", n=" << dbname << ", xg=" << test_get << ", xa=" << test_ask << ", xt=" << test_try << endl;
+  if ((argc - optind) != 1) {
+    cerr << "Must be one mandatory argument" << endl;
+    return false;
+  }
+  i = atoi(argv[optind]);
+  if ((i < 1) or (i > 31)) {
+    cerr << "Bad log2(records): " << argv[optind] << endl;
+    return false;
+  }
+  RECS_QTY = 1 << i;
+  return ((buffer = new uint160_t[RECS_QTY]));
 }
 
 inline void rand_u160(uint160_t &dst)
 {
-  /* generates 20-byte random int
-   * ~10M/s/GHz
-   */
+  /// generates 20-byte random int (~10M/s/GHz)
   for (auto i = 0; i < 5; i++)
     dst[i] = rand();
 }
@@ -67,7 +108,8 @@ uint64_t curtime(void) {
 void stage_add(function<bool (const uint160_t &, const uint32_t)> func_recadd) {
   uint160_t k;
 
-  cerr << "1. Add " << RECS_QTY << " recs ... ";
+  if (verbose)
+    cerr << "1. Add " << RECS_QTY << " recs ... ";
   srand(time(nullptr));
   auto created = 0;
   auto T0 = curtime();
@@ -79,13 +121,15 @@ void stage_add(function<bool (const uint160_t &, const uint32_t)> func_recadd) {
   }
   t1 = curtime() - T0;
   kops1 = t1 ? RECS_QTY/t1 : 0;
-  cerr << created << " / " << t1 << " ms (" << kops1 << " Kops)" << endl;
+  if (verbose)
+    cerr << created << " / " << t1 << " ms (" << kops1 << " Kops)" << endl;
 }
 
 void stage_get(function<bool (const uint160_t &, const uint32_t)> func_recget) {
   uint32_t v;
 
-  cerr << "2. Get " << TESTS_QTY << " recs ... ";
+  if (verbose)
+    cerr << "2. Get " << TESTS_QTY << " recs ... ";
   auto found = 0;
   auto T0 = curtime();
   for (uint32_t i = 0; i < TESTS_QTY; i++) {
@@ -95,14 +139,16 @@ void stage_get(function<bool (const uint160_t &, const uint32_t)> func_recget) {
   }
   t2 = curtime() - T0;
   kops2 = t2 ? TESTS_QTY/t2 : 0;
-  cerr << found << " / " << t2 << " ms (" << kops2 << " Kops)" << endl;
+  if (verbose)
+    cerr << found << " / " << t2 << " ms (" << kops2 << " Kops)" << endl;
 }
 
 void stage_ask(function<bool (const uint160_t &, const uint32_t)> func_recget) {
   uint160_t k;
   uint32_t v;
 
-  cerr << "3. Ask " << TESTS_QTY << " recs ... ";
+  if (verbose)
+    cerr << "3. Ask " << TESTS_QTY << " recs ... ";
   auto found = 0;
   auto T0 = curtime();
   for (uint32_t i = 0; i < TESTS_QTY; i++) {
@@ -118,14 +164,16 @@ void stage_ask(function<bool (const uint160_t &, const uint32_t)> func_recget) {
   }
   t3 = curtime() - T0;
   kops3 = t3 ? TESTS_QTY/t3 : 0;
-  cerr << found << " / " << t3 << " ms (" << kops3 << " Kops)" << endl;
+  if (verbose)
+    cerr << found << " / " << t3 << " ms (" << kops3 << " Kops)" << endl;
 }
 
 void stage_try(function<int (const uint160_t &, const uint32_t)> func_recgetadd) {
   uint160_t k;
   uint32_t v;
 
-  cerr << "4. Try " << TESTS_QTY << " recs ... ";
+  if (verbose)
+    cerr << "4. Try " << TESTS_QTY << " recs ... ";
   auto created = 0;
   auto found = 0;
   auto T0 = curtime();
@@ -147,7 +195,8 @@ void stage_try(function<int (const uint160_t &, const uint32_t)> func_recgetadd)
   }
   t4 = curtime() - T0;
   kops4 = t4 ? TESTS_QTY/t3 : 0;
-  cerr << found+created << " / " << t4 << " ms (" << kops4 << " Kops): " << found << " get, " << created << " add" << endl;
+  if (verbose)
+    cerr << found+created << " / " << t4 << " ms (" << kops4 << " Kops): " << found << " get, " << created << " add" << endl;
 }
 
 void out_result(void) {
